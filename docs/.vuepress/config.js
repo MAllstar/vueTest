9f0d11f9c7d1c97b86c1cc1ffbd2a55fb3c48c8c
congyu <siqing20210721@163.com>
2021-07-28 22:56:25 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: 'Gate API v4',
+  description: 'gate.io api v4.21.5',
+  base: '/docs',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Gate API v4',
+      text: 'English',
+      description: 'gate.io api v4.21.5',
+    },
+    '/zh/': {
+      lang: 'zh-hans',
+      title: 'Gate API v4',
+      text: '中文',
+      description: 'gate.io api v4.21.5',
+    },
+  },
+  themeConfig: {
+    locales: {
+      '/': {
+        selectText: 'Languages',
+        label: 'English',
+      },
+      '/zh/': {
+        selectText: '选择语言',
+        label: '简体中文',
+      },
+    },
+  },
+}
---
+++ docs/.vuepress/theme/components/Header/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="pageHeader">
+    <ul class="pageNav">
+      <li class="navItem" 
+        v-for="item in navItems" 
+        :key="item.name"
+        @mouseover="showPop(item.name)"
+        @mouseleave="hidePop()"
+      > 
+        {{ item.name }}
+        <div class="navPopup" @mouseover="showPop(item.name)">
+          <ul 
+            v-if="hoverName===item.name"  
+          >
+            <li class="popup" v-for="ll in item.list" :key="ll.name">
+              <a :href="ll.url">{{ ll.name }}</a>
+            </li> 
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: {
+    },
+    data: () => {
+      return {
+        hoverName: null,
+        navItems: [
+          {name: '现货&杠杆', list: [
+            {name: 'REST', url: '#'},
+            {name: 'WebSocket(New)', url: '#'},
+            {name: 'WebSocket(Old)', url: '#'},
+            {name: 'APIv2 REST', url: '#'},
+            {name: 'APIv2 WebSocket', url: '#'}
+          ]},
+          {name: '永续合约'},
+          {name: '交割合约'}
+        ]
+      }
+    },
+    methods: {
+      showPop(name) {
+        console.log(name)
+        this.hoverName = name
+      },
+      hidePop() {
+        this.hoverName = null
+      }
+    }
+  }
+</script>
